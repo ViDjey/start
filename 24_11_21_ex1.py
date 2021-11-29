4031c90cc6f6fec1cf84260ac718ced8f50a0703
ViDjey <94647362+ViDjey@users.noreply.github.com>
2021-11-29 18:50:47 +0300

    Update 24_11_21_ex1.py

--- 24_11_21_ex1.py
+++ 24_11_21_ex1.py
@@ -9,9 +9,7 @@ for i in range(N):
 for i in range(1,len(a)):
     for j in range(len(a)-1,i-1,-1):
         if a[j]<a[j-1]:
-            r=a[j]
-            a[j]=a[j-1]
-            a[j-1]=r
+            a[j],a[j-1]=a[j-1],a[j]
 print("Отсортированный масив")
 for i in range(len(a)):
     print(a[i])
