9dfb921d40aa607c0b8bc52643e9b26c40268259
ViDjey <94647362+ViDjey@users.noreply.github.com>
2021-12-08 20:38:12 +0300

    Update 24_11_21_ex1.py

--- 24_11_21_ex1.py
+++ 24_11_21_ex1.py
@@ -1,15 +1,15 @@
-N=int(input("Ведите размер массива для сортировки: "))
-while N<=0:
+N = int(input("Ведите размер массива для сортировки: "))
+while N <= 0:
     print("Введен неверный размер для массива")
-    N=int(input("Ведите размер массива для сортировки: "))
+    N = int(input("Ведите размер массива для сортировки: "))
 a = []
 for i in range(N):
     print(f"Введите элемент массива {i+1}: ")
     a.append(int(input()))
-for i in range(1,len(a)):
-    for j in range(len(a)-1,i-1,-1):
-        if a[j]<a[j-1]:
-            a[j],a[j-1]=a[j-1],a[j]
+for i in range(1, len(a)):
+    for j in range(len(a)-1, i-1, -1):
+        if a[j] < a[j-1]:
+            a[j], a[j-1] = a[j-1], a[j]
 print("Отсортированный масив")
 for i in range(len(a)):
     print(a[i])
