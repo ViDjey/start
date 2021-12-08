c5de1f9712a3c1065ee0ca1bd69d56c7e59bd441
ViDjey <94647362+ViDjey@users.noreply.github.com>
2021-12-08 20:45:56 +0300

    Update 29_11_21_ex3.py

--- 29_11_21/29_11_21_ex3.py
+++ 29_11_21/29_11_21_ex3.py
@@ -1,20 +1,24 @@
 def func(kol):
-        if kol==1 or kol==2:
-            return 1
-        else:
-            return func(kol-1)+func(kol-2)
-
+    """Algorithm for finding recursively a number from the Fibonacci series, under the number 'kol'
+    Keyword argument:
+    kol-type int, positive
+    """
+    if kol==1 or kol==2:
+        return 1
+    else:
+        return func(kol-1) + func(kol-2)
 
 
 tmp=True
 while tmp:
     try:
-        N=int(input("Введите нужный номер элемента ряда Фибоначчи: "))
+        N = int(input("Введите нужный номер элемента ряда Фибоначчи: "))
     except ValueError:
         print("Номер должен быть натуральное")
     else:
-        tmp=False
-        if (N<=0):
-             print("Номен должен быть положительный")
-             tmp=True
+        tmp = False
+        if (N <= 0):
+            print("Номен должен быть положительный")
+            tmp = True
 print(f"Число Фибоначчи под номером {N}: {func(N)}")
+
